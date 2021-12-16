2108706027a9d164cd5fb4c9fcdeef95cb00dd19
fecaetano630 <felipe.caetano@invillia.com>
2021-12-16 09:47:32 -0300

    primeiro commit

---
+++ src/main/java/br/com/invillia/insiders/grupo1/model/Course.java
@@ -0,0 +1,98 @@
+package br.com.invillia.insiders.grupo1.model;
+
+
+import java.time.LocalDate;
+
+public class Course {
+
+
+    private Long id;
+
+    private String category;
+
+    private double workload;
+
+    private String status;
+
+    private boolean hasCertificate;
+
+    private double evaluation;
+
+    private LocalDate createdAt;
+
+    private LocalDate updatedAt;
+
+    public Course(Long id, String category, double workload, String status) {
+        this.id = id;
+        this.category = category;
+        this.workload = workload;
+        this.status = status;
+    }
+
+    public Course(){}
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getWorkload() {
+        return workload;
+    }
+
+    public void setWorkload(double workload) {
+        this.workload = workload;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean isHasCertificate() {
+        return hasCertificate;
+    }
+
+    public void setHasCertificate(boolean hasCertificate) {
+        this.hasCertificate = hasCertificate;
+    }
+
+    public double getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(double evaluation) {
+        this.evaluation = evaluation;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
